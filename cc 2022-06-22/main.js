f62578d20d27ff43e4bd6ac71bebd13408436603
Isaiah Henson <Devpuddles@gmail.com>
2022-06-22 07:40:53 -0400

    cc challenge for 2022-06-2022

---
+++ cc 2022-06-22/main.js
@@ -0,0 +1,22 @@
+/*
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+*/
+//P: A number will be passed into the string
+//R: Return the number
+//E: function numToString(num){
+//    return num.toString()
+//}
+// numToString(5)
+//P: numberToString(num) --> return that num in string form
+
+function numberToString(num) {
+    return num.toString()
+    console.log(num)
+  }
+ numberToString(5)

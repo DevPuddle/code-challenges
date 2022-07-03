a0b2c28b71b6ef407b418c6f7e3a37478de1f1cf
Isaiah Henson <Devpuddles@gmail.com>
2022-07-03 11:28:55 -0400

    coding challenge for 2022-07-03

---
+++ cc 2022-07-03/main.js
@@ -0,0 +1,31 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+//P: An array will be passed into the function
+//R: It will square then return the added numbers 
+//E: 1^2 = 1 2^2 = 4 1 + 4 = 5
+//P: function squareSum(array) {
+// const initialValue = 0
+// const totalValue = array.reduce((a, b) => {
+//  a**a + b, initialValue
+//})
+// return initialValue;
+//}
+
+function squareSum(numbers){
+    numbers.reduce((a, b) => {
+      a + b * b
+    })
+    return a
+  }
+
+// My second attempt at this coding challenge
+
+P: function squareSum(array) {
+    const initialValue = 0
+    const totalValue = array.reduce((a, b) => {
+    a**a + b, initialValue
+    })
+    return totalValue;
+}
